8c8dfa5a37c46e0b7a65bfc3c304fdb14e756c29
danainjax <danainjax@gmail.com>
2021-09-05 16:44:24 -0400

    added punks to db, needing to find a solution for jlimiting json results, since getting 10000 records and crashing front end

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'rack-cors'
 # gem 'active_model_serializers'
 
+gem 'rest-client'
+gem 'json'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/punk.rb
+++ app/models/punk.rb
@@ -1,2 +1,18 @@
 class Punk < ApplicationRecord
+
+
+    def self.get_punks
+        params = { :limit => 10}
+       
+        response = RestClient.get("https://cryptopunks.herokuapp.com/api/punks", params)
+    #    byebug
+        punks = JSON.parse(response)
+        # puts punks
+        
+         #create a Ruby punk object with all the info in the punks array
+        punks.each do |punk|
+            self.create(punktype: punk["type"], image: punk["image"], accessories: punk["accessories"]) 
+        
+        end
+    end
 end
